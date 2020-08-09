2b8693b37f53b5e10e6528e1fc6633d715d8015a
ismet kizgin <info@ismetkizgin.com>
2020-08-10 00:26:09 +0300

    feat: added verify token

---
+++ middleware/verifyToken.js
@@ -0,0 +1,26 @@
+const jwt = require('jsonwebtoken');
+const { tokenMessage } = require('../fixtures/messageStatus.json');
+
+class VerifyToken {
+    constructor() { }
+
+    static verifyToken = (req, res, next) => {
+        const token = req.headers['token'] || req.body.token || req.query.token
+        if (token) {
+            jwt.verify(token, req.app.get('api_key'), (err, decoded) => {
+
+                if (err) {
+                    res.status(tokenMessage.Token_Invalid.status).json({ message: tokenMessage.Token_Invalid.message });
+                } else {
+                    req.decode = decoded,
+                        next();
+                }
+            });
+
+        } else {
+            res.status(tokenMessage.Token_Null.status).json({ message: tokenMessage.Token_Null.message });
+        }
+    }
+}
+
+module.exports = VerifyToken;
