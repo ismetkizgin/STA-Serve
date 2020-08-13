56f047dcef25a5f1a1189a424f2da966502f4baa
ismet kizgin <info@ismetkizgin.com>
2020-08-13 10:17:47 +0300

    feat: Authorization method added.

---
+++ middleware/authorization.js
@@ -0,0 +1,22 @@
+const TransactionsFactory = require('../database/transactionFactory');
+const authTransactions = TransactionsFactory.creating('authTransactions');
+
+class Authorization {
+    constructor() { }
+
+    static async authControl(req, res, next) {
+        try {
+            const decode = req.decode;
+            if(req.body.UserIdentityNo === req.decode.UserIdentityNo)
+                next();
+            const UserStatusTransactionName = req.originalUrl.replace('/', '');
+            const result = await authTransactions.authFindAsync({ UserStatusID: decode.UserStatusID, UserStatusTransactionName: UserStatusTransactionName });
+            if (result)
+                next();
+        } catch (error) {
+            res.status(error.status || 500).json({ message: error.message });
+        }
+    }
+}
+
+module.exports = Authorization;
