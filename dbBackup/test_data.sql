ffc590a0cbdc50292c82e3621bc8cb5e3cafd335
ismet kizgin <info@ismetkizgin.com>
2020-09-13 23:31:17 +0300

    feat: added database test data creation script

---
+++ dbBackup/test_data.sql
@@ -0,0 +1,24 @@
+--
+-- Tablo döküm verisi `tblUser`
+--
+
+INSERT INTO `tblUser` (`UserID`, `UserFirstName`, `UserLastName`, `UserPassword`, `UserIdentityNo`, `UserEmail`, `UserPhone`, `InstitutionID`, `UserStatusID`) VALUES
+(15, 'İsmet', 'Kizgin', 'password', 1, 'ismetkizgin@hotmail.com', '0533834430', 1, 1);
+
+--
+-- Tablo döküm verisi `tblUserStatus`
+--
+
+INSERT INTO `tblUserStatus` (`UserStatusID`, `UserStatusName`) VALUES
+(1, 'Root'),
+(2, 'Admin'),
+(3, 'User');
+
+--
+-- Tablo döküm verisi `tblUserStatusTransaction`
+--
+
+INSERT INTO `tblUserStatusTransaction` (`UserStatusTransactionID`, `UserStatusTransactionName`, `UserStatusID`) VALUES
+(1, 'account-delete', 1),
+(2, 'sign-up', 1);
+(3, 'user', 1);
