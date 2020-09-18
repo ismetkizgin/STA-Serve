9e0fa41a499946b02be23f0098ffc2e9e1e3a337
ismet kizgin <info@ismetkizgin.com>
2020-09-18 16:49:47 +0300

    refactor: db test data

--- dbBackup/test_data.sql
+++ dbBackup/test_data.sql
@@ -1,11 +1,4 @@
 --
--- Tablo döküm verisi `tblUser`
---
-
-INSERT INTO `tblUser` (`UserID`, `UserFirstName`, `UserLastName`, `UserPassword`, `UserIdentityNo`, `UserEmail`, `UserPhone`, `InstitutionID`, `UserStatusID`) VALUES
-(15, 'İsmet', 'Kizgin', 'password', 1, 'ismetkizgin@hotmail.com', '0533834430', 1, 1);
-
---
 -- Tablo döküm verisi `tblUserStatus`
 --
 
@@ -15,11 +8,25 @@ INSERT INTO `tblUserStatus` (`UserStatusID`, `UserStatusName`) VALUES
 (3, 'User');
 
 --
+-- Tablo döküm verisi `tblInstitution`
+--
+
+INSERT INTO `tblInstitution`(`InstitutionID`, `InstitutionName`, `InstitutionCity`, `InstitutionDistrict`, `InstitutionEmail`, `InstitutionPhone`) VALUES 
+(1,'STA Kurumu','Diyarbakır','Kayapınar','info@sta.com','05555555555')
+
+--
+-- Tablo döküm verisi `tblUser`
+--
+
+INSERT INTO `tblUser` (`UserID`, `UserFirstName`, `UserLastName`, `UserPassword`, `UserIdentityNo`, `UserEmail`, `UserPhone`, `InstitutionID`, `UserStatusID`) VALUES
+(15, 'İsmet', 'Kizgin', 'password', 1, 'ismetkizgin@hotmail.com', '0533834430', 1, 1);
+
+--
 -- Tablo döküm verisi `tblUserStatusTransaction`
 --
 
 INSERT INTO `tblUserStatusTransaction` (`UserStatusTransactionID`, `UserStatusTransactionName`, `UserStatusID`) VALUES
 (1, 'account-delete', 1),
-(2, 'sign-up', 1);
-(3, 'user', 1);
-(3, 'institution', 1);
+(2, 'sign-up', 1),
+(3, 'user', 1),
+(4, 'institution', 1);
