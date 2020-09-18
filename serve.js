00af92a6863a8f6ba254ef8ff12c7c2504538fb4
ismet kizgin <info@ismetkizgin.com>
2020-09-18 13:52:41 +0300

    perf: remove api document

--- serve.js
+++ serve.js
@@ -5,11 +5,8 @@ const routers = require('./routers');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.set('views', __dirname + '/public/views');
-app.set('view engine', 'twig');
-
 app.get('/', function (req, res) {
-    res.render('index');
+    res.json('STA Serve Project');
 });
 
 app.use(routers.authRouter);
